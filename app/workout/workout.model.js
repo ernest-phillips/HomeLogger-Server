853d366cab13d9b34a0387453e2ebec349d0c24d
Ernest Phillips <ernestlphillips@gmail.com>
2018-12-10 15:51:50 -0800

    workout model updated

--- app/workout/workout.model.js
+++ app/workout/workout.model.js
@@ -5,66 +5,48 @@
 // 4. at least one set entered
 //5. a note that can be left blank
 //6. the amount of weight used in the set
-
+// https://en.wikipedia.org/wiki/List_of_weight_training_exercises
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
 const workoutSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user"
-    },
-    exercise: {
-        type: String,
-        required: true
-    },
-    set: {
-        type: Number,
-        required: true
-    },
-    reps: {
-        type: Number,
-        required: true
-    },
-    weight: {
-        type: Number
-    },
-    date: {
-        type: Date
-    }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  exercise: { type: String, required: true },
+  set: { type: Number, required: true },
+  reps: { type: Number, required: true },
+  weight: { type: Number },
+  date: { type: Date }
 });
 
 workoutSchema.methods.serialize = function() {
-    let user;
-    if (typeof this.user.serialize === "function") {
-        user = this.user.serialize();
-    } else {
-        user = this.user;
-    }
-    return {
-        id: this._id,
-        user: user,
-        exercise: this.exercise,
-        reps: this.reps,
-        weight: this.weight,
-        date: this.date
-    };
+  let user;
+  if (typeof this.user.serialize === "function") {
+    user = this.user.serialize();
+  } else {
+    user = this.user;
+  }
+  return {
+    id: this._id,
+    user: user,
+    exercise: this.exercise,
+    reps: this.reps,
+    weight: this.weight,
+    date: this.date
+  };
 };
 
 const Workout = mongoose.model("workout", workoutSchema);
 
 const WorkoutJoiSchema = Joi.object().keys({
-    user: Joi.string().optional(),
-    exercise: Joi.string()
-        .min(1)
-        .required(),
-    reps: Joi.number()
-        .min(1)
-        .required(),
-    weight: Joi.number().min(1),
-    date: Joi.date()
+  user: Joi.string().optional(),
+  exercise: Joi.string()
+    .min(1)
+    .required(),
+  reps: Joi.number()
+    .min(1)
+    .required(),
+  weight: Joi.number().min(1),
+  date: Joi.date()
 });
 
-module.exports = {
-    Workout
-};
+module.exports = { Workout };

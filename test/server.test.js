0d12921560fc9f25230bfe440e18a579b6eb0f7a
ernest-phillips <ernestlphillips@gmail.com>
2018-12-21 06:43:51 -0800

    tests

--- test/server.test.js
+++ test/server.test.js
@@ -12,24 +12,17 @@ const {
     app
 } = require('../app/server.js');
 
-const expect = chai.expect; // So we can do "expect" instead of always typing "chai.expect"
-chai.use(chaiHttp); // implements chai http plugin
+const expect = chai.expect;
+chai.use(chaiHttp);
 
 describe('Integration tests for: /', function() {
-    // Mocha Hook: Runs before ALL the "it" test blocks.
     before(function() {
-        // Be sure to always return a promise to Mocha when doing asynchronous work,
-        // Otherwise Mocha will just asume your work is done even if it isn't.
 
-        // Starts our Express Server, so we can test it.
         return startServer(true);
     });
-    // Mocha Hook: Runs after ALL the "it" test blocks.
+    .
     after(function() {
-        // Be sure to always return a promise to Mocha when doing asynchronous work,
-        // Otherwise Mocha will just asume your work is done even if it isn't.
 
-        // Shuts down our Express Server, since we don't need it anymore.
         return stopServer();
     });
 
@@ -37,10 +30,8 @@ describe('Integration tests for: /', function() {
         chai.request(app)
             .get('/')
             .then(res => {
-                // chai-http assertions docs: http://www.chaijs.com/plugins/chai-http/#assertions 
                 expect(res).to.have.status(HTTP_STATUS_CODES.OK);
                 expect(res).to.be.html;
-                // Chai.string assertion docs: http://www.chaijs.com/api/bdd/#stringstr-msg
                 expect(res.text).to.have.string('<!DOCTYPE html>');
             });
     });

465ec4044c267cebb6ecf43881b578c77803dd2d
ernest-phillips <ernestlphillips@gmail.com>
2019-01-02 21:39:37 -0800

    config update

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 8080,
   HTTP_STATUS_CODES: {
     OK: 200,
     CREATED: 201,
@@ -9,8 +9,8 @@ module.exports = {
     NOT_FOUND: 404,
     INTERNAL_SERVER_ERROR: 500,
   },
-  // MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/fit-journal',
-  MONGO_URL: process.env.MONGO_URL || 'mongodb://ds237932.mlab.com:37932/fit-journal',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/fit-journal',
+  // MONGO_URL: process.env.MONGO_URL || 'mongodb://ds237932.mlab.com:37932/fit-journal',
   // TEST_MONGO_URL: process.env.TEST_MONGO_URL || 'mongodb://localhost:27017/test-fit-journal',
   JWT_SECRET: process.env.JWT_SECRET || 'default',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',

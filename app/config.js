83b6bb882a0a8b01a68d0799b699a0821d25cb93
Ernest <ernestlphillips@gmail.com>
2020-08-16 15:55:18 -0700

    delete unused routs

--- app/config.js
+++ app/config.js
@@ -7,9 +7,7 @@ module.exports = {
     BAD_REQUEST: 400,
     UNAUTHORIZED: 401,
     NOT_FOUND: 404,
-    INTERNAL_SERVER_ERROR: 500,
-   
-  },
+    INTERNAL_SERVER_ERROR: 500},
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   MONGO_URL:
     process.env.MONGO_URL || 'mongodb://localhost:27017/community-story',

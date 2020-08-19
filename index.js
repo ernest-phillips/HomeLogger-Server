dd23a62e2f5cbf2419afb9cba0a04be5e4cbac86
Ernest <ernestlphillips@gmail.com>
2020-08-18 22:46:54 -0700

    update npm start command

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/',(req,res) =>
     res.send(`Node and express server running on Port ${PORT}`)
     );
 
-    app.listen(PORT, () =>
-        console.log(`Your server is running on port ${PORT}`)
-    );
+app.listen(PORT, () =>
+    console.log(`Your server is running on port ${PORT}`)
+);

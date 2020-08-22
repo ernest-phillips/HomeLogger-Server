7a4a9e09b5ea6197735ff7a50c41462bf571379f
Ernest <ernestlphillips@gmail.com>
2020-08-21 20:38:38 -0700

    add middleware to router

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express' ;
+import routes from './src/routes/router';
 
 const app = express();
 const PORT = 4000;
+
+routes(app);
 
 app.get('/',(req,res) =>
     res.send(`Node and express server running on Port ${PORT}`)

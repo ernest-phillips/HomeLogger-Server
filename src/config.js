946e5ba825e94d6b457fcf00eafebce0c404824d
Ernest <ernestlphillips@gmail.com>
2020-08-29 21:44:47 -0700

    new cofig file

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};

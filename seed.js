9cedeb4166fc6e510d731b9cb8617916bfd6f55d
Ernest <ernestlphillips@gmail.com>
2020-10-01 17:12:09 -0700

    seed works with users

--- seed.js
+++ seed.js
@@ -4,40 +4,41 @@ const { MONGO_URL } = require("./src/config");
 
 const { User } = require("./src/models/user.model");
 
-const numUsers = User.estimatedDocumentCount();
-console.log(numUsers);
-// export const seedUsers = async () => {
-//   try {
-//     const quantity = 10;
-//     const users = [];
+export const seedUsers = async () => {
+  try {
+    const quantity = 10;
+    const users = [];
+    const numUsers = await User.find().estimatedDocumentCount();
 
-//     for (let u = 0; u < quantity; u++) {
-//       users.push(
-//         new User({
-//           email: faker.internet.email(),
-//           password: faker.internet.password(),
-//           username: faker.internet.userName(),
-//         })
-//       );
-//     }
+    if (numUsers >= quantity) {
+      return;
+    }
 
-//     for (const user of users) {
-//       await user.save();
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+    for (let u = 0; u < quantity; u++) {
+      users.push(
+        new User({
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+          username: faker.internet.userName(),
+        })
+      );
+    }
 
-// console.log(MONGO_URL);
+    for (const user of users) {
+      await user.save();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// async function main() {
-//   await mongoose.connect(MONGO_URL, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//   });
-//   await seedUsers();
+async function main() {
+  await mongoose.connect(MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  await seedUsers();
 
-//   await mongoose.disconnect();
-// }
-// main();
+  await mongoose.disconnect();
+}
+main();

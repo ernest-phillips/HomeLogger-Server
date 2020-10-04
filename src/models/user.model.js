4e3e9d080c7a39e89125ee6731b86cfde12adab0
Ernest <ernestlphillips@gmail.com>
2020-10-03 20:46:29 -0700

    user model as single collection for all

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,26 +1,38 @@
 const mongoose = require("mongoose");
 const { ObjectID } = require("mongodb");
 const { Home } = require("./home.model.js");
+const { v4: uuidv4 } = require("uuid");
 
 const Schema = mongoose.Schema;
+const userID = uuidv4();
 
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
+  user: {
+    first_name: { type: String },
+    last_name: { type: String },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  // References item schema
-  homes: {
-    type: Number,
-  },
+  homes: [
+    {
+      street_name: { type: String },
+      city: { type: String },
+      zip: { type: Number },
+      square_feet: { type: Number },
+      img_url: [
+        {
+          type: String,
+        },
+      ],
+      items: [
+        {
+          desc: { type: String },
+          price: { type: Number },
+          img_url: { type: String },
+        },
+      ],
+    },
+  ],
 });
 
 const User = mongoose.model("User", UserSchema);
